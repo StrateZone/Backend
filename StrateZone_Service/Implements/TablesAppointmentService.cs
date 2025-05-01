b771703228a9641964a5a2364921df06b015583f
Baronah <kimnguu2743@gmail.com>
2025-05-01 10:24:26 +0700

    fix: auto cancel and refund

--- StrateZone_Service/Implements/TablesAppointmentService.cs
+++ StrateZone_Service/Implements/TablesAppointmentService.cs
@@ -478,6 +478,7 @@ namespace StrateZone_Service.Implements
                     await service.CreateNotificationAsync(notification);
                 });
 
+                tablesAppointment.Status = AppointmentStatus.cancelled.ToString();
                 return await UpdateTablesAppointmentAsync(_mapper.Map<TablesAppointmentModel>(tablesAppointment), tablesAppointmentId);
             }
             catch (Exception ex)
