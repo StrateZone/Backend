2d95ef39d4908db32522c8d32c91338244ca658f
Baronah <kimnguu2743@gmail.com>
2025-06-03 16:20:33 +0700

    fix: requests cancel

--- StrateZone_Repository/Implements/AppointmentrequestRepository.cs
+++ StrateZone_Repository/Implements/AppointmentrequestRepository.cs
@@ -533,8 +533,8 @@ namespace StrateZone_Repository.Implements
                 "WHERE appointment_id = {0} AND table_id = {1} AND start_time = {2} AND end_time = {3}", 
                 ta.AppointmentId, 
                 ta.TableId, 
-                ta.ScheduleTime, 
-                ta.EndTime
+                DateTime.SpecifyKind(ta.ScheduleTime, DateTimeKind.Utc), 
+                DateTime.SpecifyKind(ta.EndTime, DateTimeKind.Utc)
             );
         }
 
